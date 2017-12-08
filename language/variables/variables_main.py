67d1e1fca4403e5910138e3d28111cf735e5ecca
Corey Zinn <coreyazinn@yahoo.com>
2017-12-07 21:13:23 -0500

    created variables with example of each variable type

---
+++ language/variables/variables_main.py
@@ -0,0 +1,16 @@
+"""
+this module demonstrates variable types including numbers, strings,
+lists, and dictionaries
+"""
+
+a_number = 420
+a_string = 'EDC'
+a_list = [a_number, a_string, '3D Printer', 866]
+a_dictionary = {'Location': a_string, 'Favorite Number': a_number}
+
+print(a_number)
+print(a_string)
+print(a_list)
+print(a_dictionary)
+
+
